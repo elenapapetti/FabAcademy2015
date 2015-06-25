f175743cd3fdaa56167a2281f60014fc1b32453f
elenapapetti <papetti.elena@gmail.com>
2015-06-25 17:39:25 +0200

    update files

---
+++ Week10/Input/IDE/sketch_input_apr20a/sketch_input_apr20a.ino
@@ -0,0 +1,40 @@
+//Elena Papetti
+//Fab Lab Frosinone
+//Fab Academy 2015
+
+
+// set pin numbers:
+const int buttonPin = 3;     // the number of the pushbutton pin
+const int ledPin =  7;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);      
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);     
+}
+
+
+
+void loop() {
+
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == LOW) {     
+    // turn LED on:    
+    digitalWrite(ledPin, HIGH);  
+  } 
+  else {
+    // blink
+    digitalWrite(ledPin, HIGH); 
+    delay(500);
+    digitalWrite(ledPin, LOW); 
+    delay(500);
+  }
+}
---
+++ Week7/Blink_Hello_World/Blink_Hello_World.ino
@@ -0,0 +1,17 @@
+// Elena Papetti
+// Fab Lab Frosinone
+// Fab Academy 2015
+
+
+void setup() {
+  // initialize the red led as an output.
+  pinMode(7, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(7, HIGH);   // turn the LED on 
+  delay(1000);              // wait for a second
+  digitalWrite(7, LOW);    // turn the LED off 
+  delay(1000);              // wait for a second
+}
