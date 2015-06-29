dbcc4b6c80a890eaef5b068b3f1813d0da0a67f0
elenapapetti <papetti.elena@gmail.com>
2015-06-29 19:03:48 +0200

    files final

---
+++ final project/programming/input/_4_Sensor_Flex/_4_Senor_Flex.ino
@@ -0,0 +1,115 @@
+
+// #Elena Papetti 
+// #Fablab Frosinone
+// #Giugno 2015
+
+#define sensElbowSx A0 // Gomito SX
+#define sensShoulderSx A1 // Spalla SX
+#define sensElbowDx A2  // Gomito DX
+#define sensShoulderDx A3 // Spalla DX
+
+// Reset Vars
+int valueElbowSx = 0;
+int valueShoulderSx = 0;
+int valueElbowDx = 0;
+int valueShoulderDx = 0;
+
+static int valuePrevSens = 0;
+static int valueActSens = 0;
+const int diffTrigger = 3;  // SensibilitÃ 
+
+// Sensor Calibration Value
+// A0
+int minSensValElbowSx = 30; // This value for flat position
+int maxSensValElbowSx = 90; // This value for +90 degrees
+// A1
+int minSensValShoulderSx = 10; // This value for flat position
+int maxSensValShoulderSx = 33; // This value for +90 degrees
+// A2
+int minSensValElbowDx = 52; // This value for flat position
+int maxSensValElbowDx = 102; // This value for +90 degrees
+// A3
+int minSensValShoulderDx = 11; // This value for flat position
+int maxSensValShoulderDx = 33; // This value for +90 degrees
+//
+byte const splitPositions = 10;
+void setup() {
+  Serial.begin(115200);
+  pinMode( sensElbowSx,INPUT );
+  pinMode( sensShoulderSx,INPUT );
+  pinMode( sensElbowDx,INPUT );
+  pinMode( sensShoulderDx,INPUT );
+ 
+} 
+void loop() {
+  valueElbowSx = analogRead( sensElbowSx );
+  valueShoulderSx  = analogRead( sensShoulderSx );
+  valueElbowDx   = analogRead( sensElbowDx );
+  valueShoulderDx = analogRead( sensShoulderDx ); 
+  int posElbowSx = map(valueElbowSx, minSensValElbowSx, maxSensValElbowSx, splitPositions, 0 );
+  int posShoulderSx = map(valueShoulderSx, minSensValShoulderSx, maxSensValShoulderSx, splitPositions, 0);
+  int posElbowDx = map(valueElbowDx, minSensValElbowDx, maxSensValElbowDx, splitPositions, 0);
+  int posShoulderDx = map(valueShoulderDx, minSensValShoulderDx, maxSensValShoulderDx, splitPositions, 0);
+ 
+  /*
+  
+  Serial.print( valueElbowSx );
+  Serial.print( " P: " );
+  Serial.print( posElbowSx );
+  Serial.print( " - " );
+  
+  Serial.print( valueElbowDx );
+  Serial.print( " P: " );
+  Serial.print( posElbowSx );
+  Serial.print( " - " );
+
+  Serial.print( valueShoulderSx );
+  Serial.print( " P: " );
+  Serial.print( posShoulderSx );
+  Serial.print( " - " );
+  
+  Serial.print( valueShoulderDx );
+  Serial.print( " P: " );
+  Serial.print( posShoulderDx  );
+  Serial.print( "\n" );
+  
+  delay( 1000 );
+  
+  */
+  String outString = String(posElbowSx);
+  outString += ",";
+  outString += String(posShoulderSx);
+  outString += ",";
+  outString += String(posElbowDx);
+  outString += ",";
+  outString += String(posShoulderDx);
+  
+  boolean senddata = diff(posElbowSx + posShoulderSx + posElbowDx + posShoulderDx);
+  if (senddata == true) 
+  {
+    // Serial.println(send);
+    Serial.println(outString);    // Ritardo suono successivo
+    delay(500);        // delay in between reads for stability
+  }
+    else
+  {
+    // Serial.println(send);
+    Serial.println("0,0,0,0");
+    delay(50);        // delay in between reads for stability
+  }
+
+}
+
+boolean diff(int ActValue) {
+    if (abs(valuePrevSens - ActValue) >= diffTrigger) {
+        valuePrevSens = ActValue;
+    return true;
+  } 
+  else 
+  {
+    return false;
+  } 
+} 
+
+
+
---
+++ final project/programming/output/ProgrammaSuoniAnalogic2/ProgrammaSuoniAnalogic2.pde
@@ -0,0 +1,96 @@
+// Elena Papetti
+// Fab Lab Frosinone
+// Fab Academy 2015
+
+import beads.*;
+import java.util.Arrays; 
+import processing.serial.*;
+AudioContext ac;
+Glide carrierFreq, modFreqRatio;
+ Serial myPort;        // The serial port
+ float inByte;
+void setup() {
+ println(Serial.list());
+ // I know that the first port in the serial list on my mac
+ // is always my  Arduino, so I open Serial.list()[0].
+ // Open whatever port is the one you're using.
+ myPort = new Serial(this, "COM12" ,115200);
+ // don't generate a serialEvent() unless you get a newline character:
+ myPort.bufferUntil('\n');
+
+  size(500,500);
+  ac = new AudioContext();
+  /*
+   * This is a copy of Lesson 3 with some mouse control.
+   */
+   //this time we use the Glide object because it smooths the mouse input.
+  carrierFreq = new Glide(ac, 500);
+  modFreqRatio = new Glide(ac, 1);
+  Function modFreq = new Function(carrierFreq, modFreqRatio) {
+    public float calculate() {
+      // return x[0] * x[1] ;  // MODULAZIONE A2   DX SOPA SOTTO 
+      return 0; // x[0] * x[1] ;  // MODULAZIONE A2   DX SOPA SOTTO 
+    }
+  };
+  WavePlayer freqModulator = new WavePlayer(ac, modFreq, Buffer.SINE);
+  Function carrierMod = new Function(freqModulator, carrierFreq) {
+    public float calculate() {
+      // return x[0] * inByte + x[1];  // MODULAZIONE A1   SX  DX SX
+      return inByte;  // MODULAZIONE A1   SX  DX SX
+    }
+  };
+  WavePlayer wp = new WavePlayer(ac, carrierMod, Buffer.SINE);
+  Gain g = new Gain(ac, 1, 0.1);
+  g.addInput(wp);
+  ac.out.addInput(g);
+  ac.start();
+}
+
+ void serialEvent (Serial myPort) {
+ // get the ASCII string:
+ String inString = myPort.readStringUntil('\n');
+ 
+ if (inString != null) {
+ // trim off any whitespace:
+ inString = trim(inString);
+ // convert to an int and map to the screen height:
+ String[] list = split(inString, ',');
+ 
+ inByte = float(list[0]) + float(list[1]) + float(list[2]) + float(list[3]);
+ inByte = map(inByte, 0, 40, 100, 2500);
+ 
+/*
+ * The drawing code also has some mouse listening code now.
+ */
+
+ }}
+ 
+ 
+/*
+ * The drawing code also has some mouse listening code now.
+ */
+color fore = color(255, 102, 204);
+color back = color(0,0,0);
+
+/*
+ * Just do the work straight into Processing's draw() method.
+ */
+void draw() {
+  loadPixels();
+  //set the background
+  Arrays.fill(pixels, back);
+  //scan across the pixels
+  for(int i = 0; i < width; i++) {
+    //for each pixel work out where in the current audio buffer we are
+    int buffIndex = i * ac.getBufferSize() / width;
+    //then work out the pixel height of the audio data at that point
+    int vOffset = (int)((1 + ac.out.getValue(0, buffIndex)) * height / 2);
+    //draw into Processing's convenient 1-D array of pixels
+    vOffset = min(vOffset, height);
+    pixels[vOffset * height + i] = fore;
+  }
+  updatePixels();
+  //mouse listening code here
+  carrierFreq.setValue((float)mouseX / width * 1000 + 50);
+  modFreqRatio.setValue((1 - (float)mouseY / height) * 10 + 0.1);
+}
